a4dac8afa0ac6eb2cdb51f1cadabee98744054d0
Sharm-Zhao <share_me@126.com>
2021-06-22 19:41:35 +0800

    src 目录存放 Java 代码

--- src/sharm/test/GitTry.java
+++
@@ -1,8 +0,0 @@
-package sharm.test;
-
-public class GitTry {
-    public static void main(String[] args) {
-        System.out.println("我就是想测试修改 GitHub 上自己仓库的代码");
-        System.out.println("我就是想修改一下 GitHub 自己仓库的文件夹的备注");
-    }
-}
